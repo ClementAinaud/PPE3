27d1ebca8a40308b192e741a7613194d575d41eb
cleme <cleme@PCAINAUD>
2017-10-11 11:35:50 +0200

    creation du projet

--- application/views/v_FormulaireVisiteursAc.php
+++ application/views/v_FormulaireVisiteursAc.php
@@ -10,41 +10,36 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script type="text/javascript" src="JQuery/jquery-3.1.1.js"></script>
-        <script type="text/javascript" src="<?php base_url() ?>JS/lesFonctions.js"></script>
+        <script type="text/javascript" src="JS/lesFonctions.js"></script>
          <script type="text/javascript">
          
-             $
-           (
-               function()
-                {
-                    
-                    $("#regionsFormulaire").change(function(){ 
-                         function(){
-                        ($(this).val());
-                    });
-                        
-                   
-                   
-                   
-                   
-                }
-             );
+//             
+//         $  (
+//               function()
+//                {
+//                    
+//                    $("#regionsFormulaire").change(function(){ 
+//                         function(){
+//                        ($(this).val());
+//                    });
+//                        
+//                   
+//                   
+//                   
+//                   
+//                }
+//             );
          
         </script>
     </head>
     <body>
-        <p>Matricule</p> <input type="text">
-        <p>Nom</p> <input type="text">
-         <p>Prenom</p> <input type="text">
-          <p>Adresse</p> <input type="text">
-           <p>cp</p> <input type="text">
-            <p>ville</p> <input type="text">
-             <p>Date D'embauche</p> <input type="text">
-              <p>Code secteur</p> <input type="text">
-               <p>Code L'aboratoire</p> <input type="text">
-               
-               <div id="regionsFormulaire">
-             
-               </div>
+        <?php 
+        
+     echo"<p> matricule</p> <input type='text' value='".$visiteur->VIS_MATRICULE."'>";
+        
+ 
+             ?>
+                
+        
     </body>
 </html>
